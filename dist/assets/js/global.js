ccba7b602c26e07e1419a23ccb433489dfc39db1
Soumek <franklincovarrubia20@gmail.com>
2019-06-25 17:53:24 -0400

    nav bar fix

--- dist/assets/js/global.js
+++ dist/assets/js/global.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-  $('.notifications-dropdown-toggle').focusout(function() {
-    $('.notifications-dropdown').collapse('hide');
-  });
-  $('.user-dropdown-toggle').focusout(function() {
-    $('.user-dropdown').collapse('hide');
-  });
-});
+// $(document).ready(function() {
+//   $('.notifications-dropdown-toggle').focusout(function() {
+//     $('.notifications-dropdown').collapse('hide');
+//   });
+//   $('.user-dropdown-toggle').focusout(function() {
+//     $('.user-dropdown').collapse('hide');
+//   });
+// });
