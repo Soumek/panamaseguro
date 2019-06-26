d9ccbb2f2bb8d1ceb89d41a773e899c4b7840fa1
Soumek <franklincovarrubia20@gmail.com>
2019-06-26 00:31:02 -0400

    dashboard fix

--- dist/assets/js/dashboard.js
+++ dist/assets/js/dashboard.js
@@ -2,48 +2,27 @@ $(document).ready(function() {
   const menus = [
     {
       id: 'dashboard-menu-choferes',
-      collapse: 'dropdown-choferes'
+      dropdown: 'dropdown-choferes'
     },
     {
       id: 'dashboard-menu-panama',
-      collapse: 'dropdown-panama'
+      dropdown: 'dropdown-panama'
     },
     {
       id: 'dashboard-menu-usuarios',
-      collapse: 'dropdown-usuarios'
+      dropdown: 'dropdown-usuarios'
     },
     {
       id: 'dashboard-menu-empresas',
-      collapse: 'dropdown-empresas'
+      dropdown: 'dropdown-empresas'
     }
   ];
   menus.map(menu => {
-    $(`#${menu.id}`).focusout(function() {
-      $(`#${menu.collapse}`).collapse('hide');
-    });
-    $(`#${menu.collapse}`).on('show.bs.collapse', function() {
-      //   let parent = e.target.dataset.parent;
+    $(`#${menu.dropdown}`).on('show.bs.dropdown', function() {
       $(`#${menu.id}`).addClass('dashboard-menu-item-active');
     });
-    $(`#${menu.collapse}`).on('hide.bs.collapse', function() {
-      //   let parent = e.target.dataset.parent;
+    $(`#${menu.dropdown}`).on('hide.bs.dropdown', function() {
       $(`#${menu.id}`).removeClass('dashboard-menu-item-active');
     });
   });
-
-  //   $('#dashboard-menu-choferes').focusout(function() {
-  //     $('#dropdown-choferes').collapse('hide');
-  //   });
-  //   $('#dashboard-menu-panama').focusout(function() {
-  //     $('#dropdown-panama').collapse('hide');
-  //   });
-
-  //   $('#dropdown-choferes').on('show.bs.collapse', function(e) {
-  //     let parent = e.target.dataset.parent;
-  //     $(`${parent}`).addClass('dashboard-menu-item-active');
-  //   });
-  //   $('#dropdown-choferes').on('hide.bs.collapse', function(e) {
-  //     let parent = e.target.dataset.parent;
-  //     $(`${parent}`).removeClass('dashboard-menu-item-active');
-  //   });
 });
--- dist/assets/js/global.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function() {
-//   $('.notifications-dropdown-toggle').focusout(function() {
-//     $('.notifications-dropdown').collapse('hide');
-//   });
-//   $('.user-dropdown-toggle').focusout(function() {
-//     $('.user-dropdown').collapse('hide');
-//   });
-// });
